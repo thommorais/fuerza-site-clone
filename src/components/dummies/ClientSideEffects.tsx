73fbd7cc39f565981de6d9a0f1de1c2dc5f7c9e8
thommorais <thommorais@gmail.com>
2023-02-28 12:25:35 -0300

    try a fix

--- src/components/dummies/ClientSideEffects.tsx
+++ src/components/dummies/ClientSideEffects.tsx
@@ -5,15 +5,11 @@ import TypeShuffle from '.utils/typeShuffle'
 /* eslint-disable no-magic-numbers */
 import { useLayoutEffect } from 'react'
 
-let runOnce = false
-
 function ClientSideEffects() {
 	useLayoutEffect(() => {
-		if (typeof window === 'undefined' || runOnce) return
+		if (typeof window === 'undefined' || typeof document === 'undefined') return
 
-		runOnce = true
-
-		const typeShufflesEls = [...document.querySelectorAll('.type-shuffle')]
+		const typeShufflesEls = [...(document.querySelectorAll('.type-shuffle') || [])]
 
 		typeShufflesEls.forEach((el) => {
 			const ts = new TypeShuffle(el)

295f045058f18622c557500bea187607f94d28f3
thommorais <thommorais@gmail.com>
2023-02-27 23:49:18 -0300

    move start fields for another file

--- src/components/dummies/HeaderIllustration/Stars.tsx
+++ src/components/dummies/HeaderIllustration/Stars.tsx
@@ -1,258 +1,47 @@
 'use client'
 
 /* eslint-disable no-magic-numbers */
-import { useRef, useEffect } from 'react'
+import { useRef, useLayoutEffect } from 'react'
 
-function lerp(a: number, b: number, t: number) {
-	return b * t + a * (1 - t)
-}
-const mouse = { x: 0, y: 0, down: false }
+import S from './styles/illustration.module.css'
 
-class StarField {
-	x: number
-	y: number
-	z: number
-	width: number
-	height: number
-	depth: number
-	focalLength: number
-	speed: number
-	warpMode: boolean
-	warp: number
-	warpStart: number
-	warpEnd: any
-	t: number
-	numStars: number
-	stars: { z: number; y: number; r: number; x: number }[]
-	maxRadius: number
-	minRadius: number
-	index: number
-	z_last: any
-	constructor(x: number, y: number, z: number, w: number, h: number, d: number, n: number) {
-		this.x = x
-		this.y = y
-		this.z = z
-		this.width = w
-		this.height = h
-		this.depth = d
-		this.focalLength = 5
-		this.speed = 20
+import { loopStarField } from './starsHelpers'
 
-		this.warpMode = false
-		this.warp = 0
-		this.warpStart = 0
-		this.t = 0
-
-		this.numStars = n
-		// this.density = starsPer64pxCubed / (64 * 64 * 64)
-		// this.numStars = Math.round(this.density * this.width * this.height * this.depth)
-		this.stars = []
-		this.maxRadius = 1.5
-		this.minRadius = 0.3
-		const dr = this.maxRadius - this.minRadius
-
-		// Initialize n stars
-		for (let i = 0; i < this.numStars; ++i) {
-			this.stars[i] = {
-				x: Math.random() * this.width,
-				y: Math.random() * this.height,
-				z: Math.random() * this.depth,
-				r: Math.random() * dr + this.minRadius,
-			}
-		}
-
-		// Z-sorting (only happens once)
-		this.stars.sort((a: { z: number }, b: { z: number }) => a.z - b.z)
-		this.index = 0
-	}
-
-	update(dt: number) {
-		// Move camera forward
-		this.z_last = this.z
-		this.z += (this.speed * dt) / 1000
-
-		const dr = this.maxRadius - this.minRadius
-
-		for (const star of this.stars) {
-			// Check if star is out of view
-			if (this.z - this.focalLength >= star.z) {
-				// Increment this.index
-				this.index = (this.index + 1) % this.numStars
-				// Push star back by 1 depth and randomize X, Y coordinates and radius
-				star.x = Math.random() * this.width
-				star.y = Math.random() * this.height
-				star.z += this.depth
-				star.r = Math.random() * dr + this.minRadius
-			}
-		}
-
-		let t
-		if (mouse.down) {
-			if (this.warpStart === 0) {
-				this.warpStart = Date.now()
-				t = this.t
-			} else {
-				t = (Date.now() - this.warpStart) / 1000
-			}
-
-			// MAXIMUM WARP, ENGAGE!
-			this.warpMode = true
-			this.warpEnd = 0
-			const x = t < 1 ? 3 * (1 - t) * t * t + t * t * t : 1
-			this.warp = Math.min(Math.max(0, x), 1)
-		} else {
-			if (this.warpEnd === 0) {
-				this.warpEnd = Date.now()
-				t = 0
-			} else {
-				t = (Date.now() - this.warpEnd) / 1000
-			}
-
-			const x = t < 1 ? this.warp - (3 * (1 - t) * t * t + t * t * t) : 0
-			this.warp = Math.min(Math.max(0, x), 1)
-			if (this.warp === 0) this.warpMode = false
-			this.warpStart = 0
-		}
-
-		this.speed = lerp(2.4, 24, this.warp)
-		this.focalLength = lerp(20, 10, this.warp)
-	}
-
-	draw(ctx: any) {
-		ctx.save()
-
-		// Draw background
-		ctx.fillStyle = '#050f2d'
-		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-		// Put origin in center of canvas
-		ctx.translate(0.5 * ctx.canvas.width, 0.5 * ctx.canvas.height)
-
-		const f = this.focalLength
-
-		// Stars
-		// for (let star of this.stars) {
-		for (let i = 0; i < this.numStars; ++i) {
-			// Iterate backwards starting at this.index
-			let index = this.index - 1 - i
-			if (index < 0) index += this.numStars
-			const star = this.stars[index]
-
-			// Distance from star to focal point
-			const z = star.z - this.z + f
-			if (z < 1) continue
-
-			// Pinhole camera projection transformation
-			const sx = (f * (star.x - this.x)) / z
-			const sy = (f * (star.y - this.y)) / z
-			const sr = (star.r * f) / z
-
-			// Interpolate color based on depth
-			const color = Math.floor(224 * Math.max(1 - z / this.depth, 0) + 16)
-			ctx.fillStyle = `rgb(${color},${color},${color})`
-
-			// Alpha falloff as approaching focal point
-			ctx.globalAlpha = Math.min(Math.max(0, 1 - Math.exp(-z + 1)), 1)
-
-			if (this.warpMode) {
-				// This should be correct, but produces jittery results
-				// let wf = 1.0 + 20.0 * this.warp // warp factor
-				// let z2 = star.z - (wf * (this.z_last - this.z) + this.z) + f
-
-				// This seems to be more stable
-				const wf = 1.0 + 0.2 * this.warp // warp factor
-				const z2 = wf * (star.z - this.z_last + f)
-
-				// Pinhole camera projection transformation (rear)
-				const sx2 = (f * (star.x - this.x)) / z2
-				const sy2 = (f * (star.y - this.y)) / z2
-
-				// Calculate angles to tangents
-				const x1 = sx
-				const y1 = sy
-				const x2 = sx2
-				const y2 = sy2
-				const r1 = sr
-				const r2 = (star.r * f) / z2
-				const beta = Math.asin((r2 - r1) / Math.hypot(x2 - x1, y2 - y1))
-				const a1 = Math.atan2(y1 - y2, x2 - x1) - beta
-				const a2 = Math.atan2(y2 - y1, x1 - x2) + beta
-
-				if (!isNaN(beta)) {
-					// Connect two circles by their tangents
-					ctx.beginPath()
-					ctx.arc(x1, y1, r1, Math.PI / 2 - a1, Math.PI / 2 - a2, false)
-					ctx.arc(x2, y2, r2, Math.PI / 2 - a2, Math.PI / 2 - a1, false)
-					ctx.closePath()
-					ctx.fill()
-				} else {
-					// One circle is inside the other, draw the larger circle
-					ctx.beginPath()
-					if (r1 > r2) ctx.arc(x1, y1, r1, 0, 2 * Math.PI, false)
-					else ctx.arc(x2, y2, r2, 0, 2 * Math.PI, false)
-					ctx.fill()
-				}
-			} else {
-				ctx.beginPath()
-				ctx.arc(sx, sy, sr, 0, 2 * Math.PI, false)
-				ctx.fill()
-			}
-		}
-
-		ctx.restore()
-	}
-}
+type Stars = { x: number; y: number; z: number; r: number }[]
 
 function Stars() {
 	const canvas = useRef<HTMLCanvasElement>(null)
 
-	useEffect(() => {
+	useLayoutEffect(() => {
 		if (typeof window === 'undefined' || !canvas.current) return
 
 		if (!canvas.current) return
 
+		const width = window.innerWidth
+		const height = (window.innerHeight * 98) / 100
+		canvas.current.width = width
+		canvas.current.height = height
 		const ctx = canvas.current.getContext('2d')
-		canvas.current.width = window.innerWidth
-		canvas.current.height = window.innerHeight
-
-		const sf = new StarField(0, 0, 0, 2 * canvas.current.width, 2 * canvas.current.height, 100, 1000)
-		sf.x = sf.width / 2
-		sf.y = sf.height / 2
-
-		canvas.current.addEventListener('mousedown', () => (mouse.down = true))
-		canvas.current.addEventListener('mouseup', () => (mouse.down = false))
-
-		// Reset canvas size, starfield size, and camera position on window resize
+		const loop = loopStarField({ width, height, ctx })
 
 		function onResize() {
-			if (!canvas.current) return
-			canvas.current.width = window.innerWidth
-			canvas.current.height = window.innerHeight
-			sf.width = 2 * window.innerWidth
-			sf.height = 2 * window.innerHeight
-			sf.x = sf.width / 2
-			sf.y = sf.height / 2
+			if (canvas.current) {
+				canvas.current.width = window.innerWidth
+				canvas.current.height = (window.innerHeight * 98) / 100
+			}
 		}
 
 		window.addEventListener('resize', onResize)
 
-		let t0 = Date.now() - 1 / 60
-		function loop() {
-			const dt = Date.now() - t0
-			t0 = Date.now()
-			sf.update(dt)
-			sf.draw(ctx)
-			requestAnimationFrame(loop)
-		}
-
-		requestAnimationFrame(loop)
+		const ref = requestAnimationFrame(loop)
 
 		return () => {
+			cancelAnimationFrame(ref)
 			window.removeEventListener('resize', onResize)
 		}
 	}, [])
 
-	return <canvas ref={canvas} />
+	return <canvas ref={canvas} className={S.canvas} />
 }
 
 export default Stars
---
+++ src/components/dummies/HeaderIllustration/starsHelpers.ts
@@ -0,0 +1,408 @@
+/* eslint-disable no-magic-numbers */
+
+type Stars = { x: number; y: number; z: number; r: number }[]
+
+type StarFieldParams = {
+	stars: Stars
+	maxRadius: number
+	minRadius: number
+	focalLength: number
+	zIndex: number
+	index: number
+	width: number
+	height: number
+	depth: number
+	dt: number
+	speed: number
+	warpStart: number
+	warpEnd: number
+	warp: number
+	warpMode: boolean
+	ctx: CanvasRenderingContext2D
+}
+
+function lerp(a: number, b: number, t: number) {
+	return b * t + a * (1 - t)
+}
+
+export function createStars({
+	width,
+	height,
+	depth,
+	numStars,
+}: {
+	width: number
+	height: number
+	depth: number
+	numStars: number
+}) {
+	const minRadius = 0.5
+	const maxRadius = 2
+
+	const dr = maxRadius - minRadius
+
+	const stars: { x: number; y: number; z: number; r: number }[] = []
+
+	for (let i = 0; i < numStars; ++i) {
+		stars[i] = {
+			x: Math.random() * width,
+			y: Math.random() * height,
+			z: Math.random() * depth,
+			r: Math.random() * dr + minRadius,
+		}
+	}
+
+	const sortedStars = [...stars].sort((a: { z: number }, b: { z: number }) => a.z - b.z)
+	return sortedStars
+}
+
+export function drawStars({
+	ctx,
+	stars,
+	index,
+	focalLength,
+	depth,
+	zIndex,
+	width,
+	height,
+	warp,
+	lastZIndex,
+	warpMode,
+}: {
+	ctx: CanvasRenderingContext2D
+	stars: Stars
+	index: number
+	focalLength: number
+	depth: number
+	zIndex: number
+	width: number
+	height: number
+	warp: number
+	warpMode: boolean
+	lastZIndex: number
+}) {
+	ctx.save()
+	ctx.fillStyle = '#050f2d'
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+	ctx.translate(0.5 * ctx.canvas.width, 0.5 * ctx.canvas.height)
+
+	const starsCount = stars.length
+
+	for (let i = 0; i < starsCount; ++i) {
+		let currentIndex = index - 1 - i
+
+		if (currentIndex < 0) {
+			currentIndex += starsCount
+		}
+
+		const star = stars[currentIndex]
+
+		const z = star.z - zIndex + focalLength
+
+		if (z < 1) {
+			continue
+		}
+
+		const sx = (focalLength * (star.x - width)) / z
+		const sy = (focalLength * (star.y - height)) / z
+		const sr = (star.r * focalLength) / z
+
+		const color = Math.floor(224 * Math.max(1 - z / depth, 0) + 16)
+		ctx.fillStyle = `rgb(${color}, ${color}, ${color})`
+		ctx.globalAlpha = Math.min(Math.max(0, 1 - Math.exp(-z + 1)), 1)
+
+		if (warpMode) {
+			const wf = 1.0 + 0.2 * warp
+			const z2 = wf * (star.z - lastZIndex + focalLength)
+
+			const sx2 = (focalLength * (star.x - width)) / z2
+			const sy2 = (focalLength * (star.y - height)) / z2
+
+			const x1 = sx
+			const y1 = sy
+			const x2 = sx2
+			const y2 = sy2
+			const r1 = sr
+			const r2 = (star.r * focalLength) / z2
+			const beta = Math.asin((r2 - r1) / Math.hypot(x2 - x1, y2 - y1))
+			const a1 = Math.atan2(y1 - y2, x2 - x1) - beta
+			const a2 = Math.atan2(y2 - y1, x1 - x2) + beta
+
+			if (!isNaN(beta)) {
+				ctx.beginPath()
+				ctx.arc(x1, y1, r1, Math.PI / 2 - a1, Math.PI / 2 - a2, false)
+				ctx.arc(x2, y2, r2, Math.PI / 2 - a2, Math.PI / 2 - a1, false)
+				ctx.closePath()
+				ctx.fill()
+			} else {
+				ctx.beginPath()
+				if (r1 > r2) {
+					ctx.arc(x1, y1, r1, 0, 2 * Math.PI, false)
+				} else {
+					ctx.arc(x2, y2, r2, 0, 2 * Math.PI, false)
+				}
+				ctx.fill()
+			}
+		} else {
+			ctx.beginPath()
+			ctx.arc(sx, sy, sr, 0, 2 * Math.PI, false)
+			ctx.fill()
+		}
+	}
+
+	ctx.restore()
+}
+
+export function animateStars({
+	speed,
+	stars,
+	maxRadius,
+	minRadius,
+	focalLength,
+	zIndex,
+	index,
+	width,
+	height,
+	depth,
+	dt,
+	warpEnd,
+	warpStart,
+	warp,
+	warpMode,
+}: {
+	stars: Stars
+	maxRadius: number
+	minRadius: number
+	focalLength: number
+	zIndex: number
+	index: number
+	width: number
+	height: number
+	depth: number
+	dt: number
+	speed: number
+	warpEnd: number
+	warp: number
+	warpStart: number
+	warpMode: boolean
+}) {
+	const dr = maxRadius - minRadius
+	let updatedIndex = index
+
+	const updatedZ = zIndex + (speed * dt) / 1000
+
+	const updatedStars: Stars = []
+	for (const star of stars) {
+		const updatedStar = { ...star }
+		if (updatedZ - focalLength >= star.z) {
+			updatedIndex = (updatedIndex + 1) % stars.length
+			updatedStar.x = Math.random() * width
+			updatedStar.y = Math.random() * height
+			updatedStar.r = Math.random() * dr + minRadius
+			updatedStar.z = star.z + depth
+		}
+		updatedStars.push(updatedStar)
+	}
+
+	let tr = 0
+	let updatedWarp = warp
+	let updatedWarpStart = warpStart
+	let updatedWarpEnd = warpEnd
+
+	if (warpMode) {
+		if (updatedWarpStart === 0) {
+			updatedWarpStart = Date.now()
+		} else {
+			tr = (Date.now() - updatedWarpStart) / 1000
+		}
+
+		updatedWarpEnd = 0
+		const x = tr < 1 ? 3 * (1 - tr) * tr * tr + tr * tr * tr : 1
+		updatedWarp = Math.min(Math.max(0, x), 1)
+	} else {
+		if (updatedWarpEnd === 0) {
+			updatedWarpEnd = Date.now()
+		} else {
+			tr = (Date.now() - updatedWarpEnd) / 1000
+		}
+
+		const x = tr < 1 ? warp - (3 * (1 - tr) * tr * tr + tr * tr * tr) : 0
+		updatedWarp = Math.min(Math.max(0, x), 1)
+		updatedWarpStart = 0
+	}
+
+	const updatedSpeed = lerp(2.4, 24, updatedWarp)
+	const updatedFocalLength = lerp(20, 10, updatedWarp)
+
+	return {
+		stars: updatedStars,
+		index: updatedIndex,
+		focalLength: updatedFocalLength,
+		zIndex: updatedZ,
+		speed: updatedSpeed,
+		warp: updatedWarp,
+		warpEnd: updatedWarpEnd,
+		warpStart: updatedWarpStart,
+	}
+}
+
+export function runStarField({
+	speed,
+	stars,
+	maxRadius,
+	minRadius,
+	focalLength,
+	zIndex,
+	index,
+	width,
+	height,
+	depth,
+	dt,
+	warpStart,
+	warpEnd,
+	warp,
+	warpMode,
+	ctx,
+}: StarFieldParams) {
+	const lastZIndex = zIndex
+
+	const {
+		stars: updatedStars,
+		index: updatedIndex,
+		focalLength: updatedFocalLength,
+		zIndex: updatedZ,
+		speed: updatedSpeed,
+		warp: updatedWarp,
+		warpEnd: updatedWarpEnd,
+		warpStart: updatedWrapStart,
+	} = animateStars({
+		speed,
+		stars,
+		maxRadius,
+		minRadius,
+		focalLength,
+		zIndex,
+		index,
+		width,
+		height,
+		depth,
+		dt,
+		warpStart,
+		warpEnd,
+		warp,
+		warpMode,
+	})
+
+	drawStars({
+		ctx,
+		stars,
+		index,
+		focalLength,
+		depth,
+		zIndex,
+		width: width / 2,
+		height: height / 2,
+		warp,
+		lastZIndex,
+		warpMode,
+	})
+
+	return {
+		stars: updatedStars,
+		index: updatedIndex,
+		focalLength: updatedFocalLength,
+		zIndex: updatedZ,
+		speed: updatedSpeed,
+		warp: updatedWarp,
+		warpEnd: updatedWarpEnd,
+		warpStart: updatedWrapStart,
+	}
+}
+
+export function loopStarField({
+	width,
+	height,
+	ctx,
+}: {
+	width: number
+	height: number
+	ctx: CanvasRenderingContext2D | null
+}) {
+	let index = 0
+	let focalLength = 10
+	let zIndex = 0
+	let speed = 2
+	let warp = 20
+	let warpEnd = 4
+	let warpStart = 2
+
+	const maxRadius = 0.8
+	const minRadius = 0.04
+	const depth = 100
+
+	let stars: Stars = createStars({
+		width,
+		height,
+		depth,
+		numStars: 1000,
+	})
+
+	let t0 = Date.now() - 1 / 60
+	let warpMode = false
+	function startWarpMpde() {
+		warpMode = true
+	}
+
+	function stopWarpMode() {
+		warpMode = false
+	}
+
+	window.addEventListener('mousedown', startWarpMpde)
+	window.addEventListener('mouseup', stopWarpMode)
+
+	function loop() {
+		if (ctx) {
+			const {
+				stars: updatedStars,
+				index: updatedIndex,
+				focalLength: updatedFocalLength,
+				zIndex: updatedZ,
+				speed: updatedSpeed,
+				warp: updatedWarp,
+				warpEnd: updatedWarpEnd,
+				warpStart: updatedWrapStart,
+			} = runStarField({
+				speed,
+				stars,
+				maxRadius,
+				minRadius,
+				focalLength,
+				zIndex,
+				index,
+				width,
+				height,
+				depth,
+				dt: Date.now() - t0,
+				warpStart,
+				warpEnd,
+				warp,
+				warpMode,
+				ctx,
+			})
+
+			t0 = Date.now()
+			speed = updatedSpeed
+			stars = updatedStars
+			index = updatedIndex
+			focalLength = updatedFocalLength
+			zIndex = updatedZ
+			warp = updatedWarp
+			warpEnd = updatedWarpEnd
+			warpStart = updatedWrapStart
+			requestAnimationFrame(loop)
+		}
+	}
+
+	return loop
+}
